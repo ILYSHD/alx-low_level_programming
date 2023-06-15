275ca2a68a37204fb095d2f0894a18193ae4028f
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-15 14:45:49 -0700

    adding the task 4 script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 # include <stdio.h>
-int main (void)
+/**
+ * Program to print a line using puts function
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
