8e50b7e239dda11e5aa00deda61eae6c4552d30d
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-15 15:34:33 -0700

    adding the task 5script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-/*
- * main - a program to print a line usinf printf
+#include <stdio.h>
+
+/**
+ * main - a program to print a line using printf functioin
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
