0410a41d72506e9b756ef05959d6ecf79f578011
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-17 11:33:58 -0700

    adding the task 2 script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print a line using printf functioin
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
