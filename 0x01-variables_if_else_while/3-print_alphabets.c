4d8f4433e02c36edcf9a3f3e822315566f52d32f
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-17 11:41:12 -0700

    adding the task 3 script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print a line using printf functioin
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
