83207f4c808b882ed7e480ec355dd3c5b27c2412
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-18 06:37:54 -0700

    adding the task 5 script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print all the alphabet except q and e
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; ++d)
+	{
+		printf("%d", d);
+	}
+	printf("\n");
+}
