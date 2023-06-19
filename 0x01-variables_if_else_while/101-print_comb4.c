1a31a1e9b3fbcc59f623e1c00549da3e8af05d96
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-19 13:32:33 -0700

    adding the task 11 script

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print all combinations of three digits
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 55; ++i)
+	{
+		for (j = i + 1; j <= 57; ++j)
+		{
+			for (k = j + 1; k <= 57; ++k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
