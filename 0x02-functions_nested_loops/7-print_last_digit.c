29c982686621a4a49e7fb75c98da3d10863600ab
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-21 08:42:14 -0700

    adding the task 7 script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - a program toprint last digit
+ * @n: the parametre of our function
+ * Return: 0 or 1 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int result;
+
+	result = n % 10;
+	return (result);
+}
