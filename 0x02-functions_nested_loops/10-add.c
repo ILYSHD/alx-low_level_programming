b4fc415dfdcaf0efa7af22224cb039d6a03675ea
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-21 14:59:33 -0700

    adding the task 10 script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - a program that adds two numbers
+ * @n: first number
+ * @m: second number
+ * Return: Integr value
+ */
+
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+	return (result);
+}
