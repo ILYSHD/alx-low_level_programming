c1daf1db7305f7ced21ecb7becf8a0fbcf9b294a
ILYSHD <ilyashaddaoui0@gmail.com>
2023-06-22 10:59:39 -0700

    adding the task 2 script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digit
+ * @a: The first parametre
+ * @b: the second parametre
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
