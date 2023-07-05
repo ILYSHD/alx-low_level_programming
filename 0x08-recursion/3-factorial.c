17e7648906304ff3a9b84fc5ecd2fa185a7c7711
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-05 21:13:06 +0000

    adding task 3 script

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - function
+ * @n: parametre
+ * Return: factorial of int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
