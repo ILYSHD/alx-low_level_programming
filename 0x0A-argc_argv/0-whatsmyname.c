24508dde06e8a7a601209b426979563bc89de943
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-09 20:46:11 +0000

    adding task 0 script

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: parametre
+ * @argv: parametre
+ *Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
