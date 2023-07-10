1d6c7fded84b57541f16e6b5c6d29d3539a8827c
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-10 20:21:35 +0000

    adding task 1 script

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function
+ * @str: parametre
+ *Return: pointer
+ */
+char *_strdup(char *str)
+{
+	unsigned int len = 0;
+	unsigned int i = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[len] != '\0'; ++len)
+	;
+	s = malloc(len * sizeof(*str) + 1);
+	if (s == 0)
+		return (NULL);
+	else
+	{
+		for (; i < len; i++)
+		{
+			s[i] = str[i];
+		}
+		return (s);
+	}
+}
