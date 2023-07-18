52aac6d30fea7fa0810019a775854a14d86e1ff6
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-18 21:51:21 +0000

    adding task 4 script

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,8 +49,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < len1; i++)
 		d->name[i] = name[i];
+	d->name[i] = '\0';
 	for (j = 0; j < len2; j++)
 		d->owner[j] = owner[j];
+	d->owner[j] = '\0';
 	d->age = age;
 	return (d);
 }
