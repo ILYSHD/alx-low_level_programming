e6b58ea470b0b994e1ba8e5c2d4c866e58ef967a
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-18 12:53:08 +0000

    adding task 1 script

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - function
@@ -9,6 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
