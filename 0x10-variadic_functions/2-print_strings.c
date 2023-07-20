d1c92765244181ec7e3eb86a4ab32dacf7c02384
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-20 19:39:09 +0000

    adding task 2 script

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		return;
 	va_start(ap, n);
 	for (i = 0; i < n ; i++)
-	{	
+	{
 		str = va_arg(ap, char *);
 		printf("%s", str ? str : "(nil)");
 		if (i != n - 1 && separator != NULL)
