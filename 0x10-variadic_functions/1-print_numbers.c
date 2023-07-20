3670777e013b7153ee606a1230c4cb04d9a9f2ac
ILYSHD <ilyashaddaoui0@gmail.com>
2023-07-20 10:39:45 +0000

    adding task 1 script

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_numbers - function
+ *@separator: parametre
+ *@n: parametre
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+	va_start(ap, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		printf("%s", separator);
+	}
+	printf("%d", va_arg(ap, int));
+	printf("\n");
+}
