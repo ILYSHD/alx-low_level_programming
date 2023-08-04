80132810fb357172a5f08220ee50a7480226bb30
ILYSHD <ilyashaddaoui0@gmail.com>
2023-08-04 02:54:14 -0700

    adding task 3 script

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit - a function to set bite at an index
+ * @n: parametre
+ * @index: parametre
+ *Return: 1 if it worked -1 if not
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
+}
