db05245b5a1dde001c8801e70644bcf5f2747ca3
ILYSHD <ilyashaddaoui0@gmail.com>
2023-08-07 15:42:15 -0700

    adding task 1 script

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fo, fw, len = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-		while (*text_content != '\0')
-		{
+		for (len = 0; text_content[len];)
 			len++;
-			text_content++;
-		}
 	}
-	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fo == -1)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
-	fw = write(fo, text_content, len);
-	if (fw == -1)
-		return (-1);
-	close(fo);
+
+	close(fd);
+
 	return (1);
 }
