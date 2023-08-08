2a8f2daf490cd4253c99651d08bb83f7a1d941d5
ILYSHD <ilyashaddaoui0@gmail.com>
2023-08-08 10:16:40 -0700

    adding taskk 3 script

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ *@argc: parametre
+ *@argv: parametre
+ *Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int ofile_from, ofile_to, rfile_from, wfile_to, cfile_from, cfile_to;
+	char buffer[1024];
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	ofile_from = open(argv[1],O_RDONLY);
+	rfile_from = read(ofile_from, buffer, 1024);
+	do
+	{
+		if (ofile_from == -1 || rfile_from == -1)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		ofile_to = open(argv[2],O_CREAT | O_RDWR | O_TRUNC, 0662);
+		wfile_to = write(ofile_to, buffer, rfile_from);
+		if (ofile_to == -1 || wfile_to == -1)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		rfile_from = read(ofile_from, buffer, 1024);
+		ofile_to = open(argv[2],O_WRONLY | O_APPEND);
+	} while(rfile_from > 0);
+	cfile_from = close(ofile_from);
+	cfile_to = close(ofile_to);
+	if (cfile_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ofile_from);
+		exit(100);
+	}
+	if (cfile_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ofile_to);
+		exit(100);
+	}
+	return (0);
+}
